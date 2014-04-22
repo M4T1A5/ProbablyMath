92d2def952cf1c233084ba6497ef58d254da0770
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-04-22 22:55:04 +0300

    Initial tests

--- build/vs2010/Tests/source/main.cpp
+++ build/vs2010/Tests/source/main.cpp
@@ -1,13 +1,24 @@
 #include <PMath/Vector2.hpp>
 
+#include <iostream>
+
 using namespace pmath;
 
 int main()
 {
-	Vec2 vec;
+	Vec2 vec1, vec2;
 
-	vec.x = 5;
-	vec.y = 10;
+	vec1.x = 5;
+	vec1.y = 10;
+
+	vec2.x = 10;
+	vec2.y = 5;
+
+	Vec2 vec3 = vec1 - vec2;
+
+	// Stop the program
+	std::cout << "Press any key to continue..." << std::endl;
+	std::cin.get();
 
 	return 0;
 }
