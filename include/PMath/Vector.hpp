7dd04f75f35a14c6a6dc5382190b2b7574e4b015
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-04-22 13:26:25 +0300

    Initial project structure

---
+++ include/PMath/Vector.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef VECTOR_PMATH_H
+#define VECTOR_PMATH_H
+
+namespace pmath
+{
+	template<typename Type, unsigned short Count>
+	class Vector
+	{
+
+	};
+}
+
+#include <PMath/inl/Vector.inl>
+#endif
