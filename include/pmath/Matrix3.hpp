54ed6e3a9f4689a7ff272f1d21aa0dc85b1a274c
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-04-29 01:38:45 +0300

    Added some more skeleton

--- include/pmath/Matrix3.hpp
+++ include/pmath/Matrix3.hpp
@@ -9,7 +9,16 @@ namespace pmath
     template<typename T>
     class Matrix3
     {
+    public:
+
+        static const unsigned int COLUMNS = 3;
+        static const unsigned int ROWS = 3;
     };
+
+    typedef Matrix3<float>          Mat3;
+    typedef Matrix3<double>         Mat3d;
+    typedef Matrix3<int>            Mat3i;
+    typedef Matrix3<unsigned int>   Mat3u;
 }
 
 #include <pmath/inl/Matrix3.inl>
--- include/pmath/Matrix4.hpp
+++ include/pmath/Matrix4.hpp
@@ -9,7 +9,16 @@ namespace pmath
     template<typename T>
     class Matrix4
     {
+    public:
+
+        static const unsigned int COLUMNS = 4;
+        static const unsigned int ROWS = 4;
     };
+
+    typedef Matrix4<float>          Mat4;
+    typedef Matrix4<double>         Mat4d;
+    typedef Matrix4<int>            Mat4i;
+    typedef Matrix4<unsigned int>   Mat4u;
 }
 
 #include <pmath/inl/Matrix4.inl>
