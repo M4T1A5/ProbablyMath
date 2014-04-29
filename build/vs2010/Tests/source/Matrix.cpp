54804ffc10546cec5da9acad797f2ea97ab8f454
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-04-30 00:06:15 +0300

    Matrix4 implementation

--- build/vs2010/Tests/source/Matrix.cpp
+++ build/vs2010/Tests/source/Matrix.cpp
@@ -1,5 +1,7 @@
 #include <catch.hpp>
 
+#include  <Windows.h>
+
 #include <pmath/Matrix.hpp>
 
 using namespace pmath;
@@ -122,5 +124,120 @@ TEST_CASE("Matrix3 tests", "[matrix]")
 // Matrix4
 TEST_CASE("Matrix4 tests", "[matrix]")
 {
+    REQUIRE(Mat4::identity == Mat4());
 
+    SECTION("Determinant")
+    {
+        Mat4 mat1(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+        CHECK(mat1.determinant() == 0);
+    }
+
+    SECTION("Boolean tests")
+    {
+        Mat4 mat1(1, 2, 3, 4,
+                  5, 6, 7, 8,
+                  9, 10, 11, 12,
+                  13, 14, 15, 16);
+        Mat4 mat2(5, 2, 7, 6,
+                  3, 7, 9, 2,
+                  4, 9, 2, 4,
+                  9, 5, 8, 2);
+
+        CHECK(mat1 != mat2);
+        CHECK_FALSE(mat1 != mat1);
+        CHECK(mat1 == mat1);
+        CHECK_FALSE(mat1 == mat2);
+    }
+
+    SECTION("Basic operators")
+    {
+        Mat4 mat1(1, 2, 3, 4,
+                  5, 6, 7, 8,
+                  9, 10, 11, 12,
+                  13, 14, 15, 16);
+
+        Mat4 mat2(5, 2, 7, 6,
+                  3, 7, 9, 2,
+                  4, 9, 2, 4,
+                  9, 5, 8, 2);
+
+        std::cout << mat1 << std::endl;
+
+        CHECK((mat1 - mat1) == Mat4(0, 0, 0, 0));
+        CHECK((mat1 - mat2) == Mat4(-4, 0, -4, -2,
+                                    2, -1, -2, 6,
+                                    5, 1, 9, 8,
+                                    4, 9, 7, 14));
+
+        CHECK((mat1 * mat2) == Mat4(59, 63, 63, 30,
+                                    143, 155, 167, 86,
+                                    227, 247, 271, 142,
+                                    311, 339, 375, 198));
+        Mat4 mat3 = mat1;
+        CHECK((mat3 *= mat2) == Mat4(59, 63, 63, 30,
+                                     143, 155, 167, 86,
+                                     227, 247, 271, 142,
+                                     311, 339, 375, 198));
+
+        Vec2 vec2(5, 8);
+        Vec3 vec3(7, 8, 9);
+        Vec4 vec4(6, 4, 2, 6);
+        CHECK((mat2 * vec2) == Vec2(47, 73));
+        CHECK((mat2 * vec3) == Vec3(120, 160, 122));
+        CHECK((mat2 * vec4) == Vec4(88, 76, 88, 102));
+
+        CHECK((mat3 = Mat4()) == Mat4());
+        CHECK((mat3 += mat1) == (Mat4() + mat1));
+        mat3 = Mat4();
+        CHECK((mat3 -= mat1) == (Mat4() - mat1));
+        CHECK((mat1 * 2) == Mat4(2, 4, 6, 8,
+                                 10, 12, 14, 16,
+                                 18, 20, 22, 24,
+                                 26, 28, 30, 32));
+        CHECK((mat1 / 2) == Mat4(.5f, 1, 1.5f, 2,
+                                 2.5f, 3, 3.5f, 4,
+                                 4.5f, 5, 5.5f, 6,
+                                 6.5f, 7, 7.5f, 8));
+        CHECK(Mat4i(Mat4(1, 2, 3, 4)) == Mat4i(1, 2, 3, 4));
+    }
+
+    SECTION("Inverse, transpose")
+    {
+        Mat4 mat(5, 2, 7, 6,
+                  3, 7, 9, 2,
+                  4, 9, 2, 4,
+                  9, 5, 8, 2);
+
+        CHECK(mat.inverse() == (1.f/2580.f * Mat4(-54, -330, 42, 408,
+                                    -196, 140, 248, -48,
+                                    68, 320, -244, -36, 
+                                    461, -145, 167, -282)));
+
+        CHECK(mat.transpose() == Mat4(5, 3, 4, 9,
+                                      2, 7, 9, 5,
+                                      7, 9, 2, 8,
+                                      6, 2, 4, 2));
+    }
+
+    SECTION("Speed tests")
+    {
+        Mat4 mat(5, 2, 7, 6,
+            3, 7, 9, 2,
+            4, 9, 2, 4,
+            9, 5, 8, 2);
+
+        LARGE_INTEGER start, end;
+        LARGE_INTEGER f;
+        QueryPerformanceFrequency(&f);
+        for (size_t i = 0; i < 5; ++i)
+        {
+            QueryPerformanceCounter(&start);
+            for (size_t i = 0; i < 10000; ++i)
+            {
+                mat * mat;
+            }
+            QueryPerformanceCounter(&end);
+            std::cout << "Elapsed time: " << double(end.QuadPart - start.QuadPart)/f.QuadPart << std::endl;
+        }
+    }
 }
--- include/pmath/Matrix4.hpp
+++ include/pmath/Matrix4.hpp
@@ -14,9 +14,68 @@ namespace pmath
     class Matrix4
     {
     public:
+        Matrix4();
+        Matrix4(const T& a11, const T& a12, const T& a13, const T& a14,
+                const T& a21, const T& a22, const T& a23, const T& a24,
+                const T& a31, const T& a32, const T& a33, const T& a34,
+                const T& a41, const T& a42, const T& a43, const T& a44);
+        Matrix4(const Vector4<T>& row1, const Vector4<T>& row2, const Vector4<T>& row3, const Vector4<T>& row4);
+        Matrix4(const Matrix4<T>& matrix);
+        template<typename T2>
+        Matrix4(const Matrix4<T2>& matrix);
+        ~Matrix4();
+
+        static const Matrix4<T> identity;
+
+        T determinant() const;
+
+        Matrix4<T> transpose() const;
+        static Matrix4<T> transpose(const Matrix4<T>& matrix);
+
+        Matrix4<T> cofactor() const;
+        static Matrix4<T> cofactor(const Matrix4<T>& matrix);
+
+        Matrix4<T> inverse() const;
+        static Matrix4<T> inverse(const Matrix4<T>& matrix);
+        
+        const T* ptr() const;
+
+        #pragma region Operators
+        // Comparison
+        bool operator ==(const Matrix4<T>& right) const;
+        bool operator !=(const Matrix4<T>& right) const;
+
+
+        // Assingment
+        Matrix4<T>& operator =(const Matrix4<T>& right);
+        Matrix4<T>& operator +=(const Matrix4<T>& right);
+        Matrix4<T>& operator -=(const Matrix4<T>& right);
+        Matrix4<T>& operator *=(const Matrix4<T>& right);
+        Matrix4<T>& operator *=(const T& right);
+        Matrix4<T>& operator /=(const T& right);
+
+
+        // Arithmetic
+        Matrix4<T> operator +(const Matrix4<T>& right) const;
+        Matrix4<T> operator -(const Matrix4<T>& right) const;
+        Matrix4<T> operator *(const Matrix4<T>& right) const;
+        Matrix4<T> operator *(const T& right) const;
+        Vector2<T> operator *(const Vector2<T>& right) const;
+        Vector3<T> operator *(const Vector3<T>& right) const;
+        Vector4<T> operator *(const Vector4<T>& right) const;
+        Matrix4<T> operator /(const T& right) const;
+
+
+        // Member access
+        Vector4<T>& operator [](const unsigned int index);
+        const Vector4<T>& operator [](const unsigned int index) const;
+        #pragma endregion
 
         static const unsigned int COLUMNS = 4;
         static const unsigned int ROWS = 4;
+
+    private:
+        Vector4<T> r1, r2, r3, r4;
     };
 
     typedef Matrix4<float>          Mat4;
